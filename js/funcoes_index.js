da22c1d251157b773d78159f207d2b1b8c73da28
NicolasFune <nicolas290@live.com>
2021-11-11 19:38:27 -0300

    Inicio de Projeto

---
+++ js/funcoes_index.js
@@ -0,0 +1,89 @@
+prevSlide.addEventListener('click',function VoltarSlide(){
+    
+    leftPositionSlide = leftPositionSlide + 100
+    if(leftPositionSlide > 0){
+        leftPositionSlide = -100
+    }
+    Array.from(slides).forEach(function name(params) {
+        slides.item(index).style.left = leftPositionSlide + 'vw'
+        index = index + 1
+    })
+    index=0
+    switch (leftPositionSlide) {
+        case -100:
+            dot_01.style.backgroundColor = 'rgb(131, 131, 131)'
+            dot_02.style.backgroundColor = 'black'
+            break;
+        default:
+            dot_01.style.backgroundColor = 'black'
+            dot_02.style.backgroundColor = 'rgb(131, 131, 131)'
+            break;
+    }
+})
+nextSlide.addEventListener('click',function AvançarSlide(){
+    leftPositionSlide = leftPositionSlide - 100
+    if(leftPositionSlide < -100){
+        leftPositionSlide = 0
+    }
+    Array.from(slides).forEach(function name(params) {
+        slides.item(index).style.left = leftPositionSlide + 'vw'
+        index = index + 1
+    })
+    index=0
+    switch (leftPositionSlide) {
+        case -100:
+            dot_01.style.backgroundColor = 'rgb(131, 131, 131)'
+            dot_02.style.backgroundColor = 'black'
+            break;
+        default:
+            dot_01.style.backgroundColor = 'black'
+            dot_02.style.backgroundColor = 'rgb(131, 131, 131)'
+            break;
+    }
+})
+
+dot_01.addEventListener('click',function SelecionarSlide01() {
+    dot_01.style.backgroundColor = 'black'
+    dot_02.style.backgroundColor = 'rgb(131, 131, 131)'
+    Array.from(slides).forEach(function name(params) {
+        slides.item(index).style.left= '0vw'
+        index = index + 1
+    })
+    index=0
+})
+dot_02.addEventListener('click',function SelecionarSlide02() {
+    dot_01.style.backgroundColor = 'rgb(131, 131, 131)'
+    dot_02.style.backgroundColor = 'black'
+    Array.from(slides).forEach(function name(params) {
+        slides.item(index).style.left= '-100vw'
+        index = index + 1
+    })
+    index=0
+})
+let TornarControlesVisiveis = function(){
+    for(let i=0;i<carrosel_controls.length;i++){
+        carrosel_controls[i].style.opacity = "1"
+    }
+    Array.from(slides).forEach(function name(params) {
+        slides.item(index).style.animation = 'none'
+        slides.item(index).left = '0vw'
+        index = index + 1
+    })
+    index=0
+}
+let TornarControlesInvisiveis = function(){
+    for(let i=0;i<carrosel_controls.length;i++){
+        carrosel_controls[i].style.opacity = "0"
+    }
+    Array.from(slides).forEach(function name(params) {
+        slides.item(index).style.animation = 'slide_img 10s infinite ease-in'
+        index = index + 1
+    })
+    index=0
+}
+Array.from(mouse__interaction__areas).forEach(function name(item) {
+    item.addEventListener('mouseover', TornarControlesVisiveis)
+})
+Array.from(mouse__interaction__areas).forEach(function name(item) {
+    item.addEventListener('mouseout', TornarControlesInvisiveis)
+})
