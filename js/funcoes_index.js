78adf9d35453d369388ff834e3845d55d841885a
NicolasFune <nicolas290@live.com>
2021-11-26 19:23:26 -0300

    erg

--- js/funcoes_index.js
+++ js/funcoes_index.js
@@ -119,8 +119,6 @@ window.addEventListener('resize',function (param) {
 if(window.innerWidth < 1024){
     $('#img_01').attr('src', 'img/Stories SONHOS.png');
     $('#img_02').attr('src', 'img/Stories PLANETA.png');
-
-    
 }
 let img_carrosel_height = $('.img__carrosel').height();
-    $('.carrosel').height(img_carrosel_height);
+    $('.sec_carrosel').height(img_carrosel_height);
